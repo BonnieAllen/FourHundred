4b287d2553126d6f11792a4c5aa884443bee2524
Bonnie <aalle.allen@gmail.com>
2015-11-02 11:17:54 -0500

    Changed add comment functionality, no more js and ajax, returns one comment at a time

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,37 +1,25 @@
 class CommentsController < ApplicationController
  before_action :set_post
 
-   def create
-    @comment = @post.comments.build(comment_params)
-    @comment.user_id = current_user.id
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
 
-    if @comment.save
-      respond_to do |format|
-        format.html { redirect_to root_path }
-        format.js
-      end
-    else
-      flash[:alert] = 'Check the comment form, your fresh comment was not posted.'
-      render root_path
-    end
+    redirect_to post_path(@post)
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
+    @comment.destroy
 
-    if @comment.user_id == current_user.id
-      @comment.destroy
-      respond_to do |format|
-        format.html { redirect_to root_path }
-        format.js
-      end
-    end
+    redirect_to post_path(@post)
   end
  
  private
 
  def comment_params  
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :post_id)
  end
 
  def set_post  
